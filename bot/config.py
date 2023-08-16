177e76d405c4753c8c89140bf2dbebe7e9da98f3
ZxScripter <131220168+ZxScripter@users.noreply.github.com>
2023-08-16 10:38:16 +0530

    config.py

--- bot/config.py
+++ bot/config.py
@@ -4,13 +4,13 @@
 from decouple import config
 
 try:
-    APP_ID = config("26376042", cast=int)
-    API_HASH = config("1f5343b0646645ca1eaf7c4759fc248f")
-    BOT_TOKEN = config("6491987934:AAFd5aEByfzMbUnpTgzezQNAq83D5kQY-Bg")
+    APP_ID = config("APP_ID", cast=int)
+    API_HASH = config("API_HASH")
+    BOT_TOKEN = config("BOT_TOKEN")
     DEV = 1664850827
-    OWNER = config("2036803347")
+    OWNER = config("OWNER")
     ffmpegcode = ["-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By TGVid-Comp (https://github.com/Zylern/TGVid-Comp)' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
-    THUMBNAIL = config("THUMBNAIL", default="https://te.legra.ph/file/86e958f9fc0d7cbdf1a28.jpg")
+    THUMBNAIL = config("THUMBNAIL", default="https://telegra.ph/file/f9e5d783542906418412d.jpg")
 except Exception as e:
     print("Environment vars Missing! Exiting App.")
     print(str(e))
