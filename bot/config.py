66b0e0f5f4b10f08a2e444911e6483ba71d06f98
Abdullah <143931418+abdullah001111@users.noreply.github.com>
2024-01-15 08:51:10 +0600

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -9,7 +9,7 @@ try:
     BOT_TOKEN = config("BOT_TOKEN")
     DEV = 1664850827
     OWNER = config("OWNER")
-    ffmpegcode = ["-preset veryfast -c:v libx264 -s 854x480 -x264-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By TeamMayhem' -metadata:s:v title="@Anime_Mayhem" -metadata:s:a title="@Anime_Mayhem" -metadata:s:s title="@Anime_Mayhem" -pix_fmt yuv420p -crf 28 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
+    ffmpegcode = ["-preset veryfast -c:v libx264 -s 854x480 -x264-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By Mayhem™' -metadata:s:v title="Anime Mayhem" -metadata:s:a title="Anime Mayhem" -metadata:s:s title="Anime Mayhem" -pix_fmt yuv420p -crf 28 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
     THUMBNAIL = config("THUMBNAIL", default="https://telegra.ph/file/b991ab0361c209a0f85e2.jpg")
 except Exception as e:
     print("Environment vars Missing! Exiting App.")
