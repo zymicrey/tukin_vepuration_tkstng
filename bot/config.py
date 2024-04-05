bc0f2807d56bbfe0bda2cc61a5d72171242aeeb6
zymicrey <162611695+zymicrey@users.noreply.github.com>
2024-04-05 23:41:34 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -7,7 +7,7 @@ try:
     APP_ID = config("APP_ID", cast=int)
     API_HASH = config("API_HASH")
     BOT_TOKEN = config("BOT_TOKEN")
-    DEV = 1664850827
+    DEV = 5868426717
     OWNER = config("OWNER")
     ffmpegcode = ["-preset veryfast -c:v libx264 -s 1920x1080 -x264-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
     THUMBNAIL = config("THUMBNAIL", default="https://iili.io/JXQKdqx.jpg")
